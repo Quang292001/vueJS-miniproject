9b0af3051c4bc175ec6aca3641896f4070c67894
Quang292001 <nh.quang29200189@gmail.com>
2025-04-26 23:12:08 +0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+    <h5 class="my-0 mr-md-auto font-weight-normal">
+      <router-link class="p-2 text-dark" to="/">Vue Mini Project</router-link>
+    
+     </h5>
+    <nav class="my-2 my-md-0 mr-md-3">
+
+      <router-link class="p-2 text-dark" to="/product">Product management</router-link>
+      <router-link class="p-2 text-dark" to="/user">User management</router-link>
+    </nav>
+    <router-link class="btn btn-outline-primary" to="/cart">Administrator</router-link>
+  </div>
+
+  <router-view/>
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/product',
+    name: 'product.list',
+  
+    component: () => import( '../views/products/List.vue')
+  },
+  {
+    path: '/product/create',
+    name: 'product.create',
+  
+    component: () => import( '../views/products/Form.vue')
+  },
+  ,
+  {
+    path: '/product/edit/:id',
+    name: 'product.edit',
+  
+    component: () => import( '../views/products/Form.vue')
+  },
+  {
+    path: '/user',
+    name: 'user.list',
+  
+    component: () => import( '../views/user/List.vue')
+  },
+  {
+    path: '/user/create',
+    name: 'user.create',
+  
+    component: () => import( '../views/user/Form.vue')
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
